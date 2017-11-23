95192a8e3ec44c9d8bb8cec214bd73dc875f7a3b
hak8or <hak8or@gmail.com>
2017-11-22 23:41:07 -0500

    Added attempt to change locale to en us utf-8

--- arch_setup.sh
+++ arch_setup.sh
@@ -63,6 +63,14 @@ pacman -Syu --noconfirm > $LOGFILE 2>&1
 echo "$TAGSTR Installing base-devel, git, htop, vim, rsync, and cowsay"
 pacman -Syu base-devel git htop vim cowsay rsync --noconfirm --needed > $LOGFILE 2>&1
 
+# Change locale to EN US UTF-8
+echo "$TAGSTR Changing locale to EN US UTF-8"
+sed -i 's/if (( EUID == 0 )); then/if (( 0 )); then/' /usr/bin/makepkg > $LOGFILE 2>&1
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen > $LOGFILE 2>&1
+locale-gen > $LOGFILE 2>&1
+echo LANG=en_US.UTF-8 > /etc/locale.conf > $LOGFILE 2>&1
+export LANG=en_US.UTF-8
+
 # Install Yaourt. Why yaourt instead of pacaur? Because pacaur doesn't allow
 # itself to be ran as root, even though all we have is root in the container,
 # and I don't want to bother fiddling with users just for this. Yaourt on the
